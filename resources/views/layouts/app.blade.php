30bbd80ba7950b2b0223b77e658362d4aaf91aa6
Diandreas <127276266+Diandreas@users.noreply.github.com>
2024-06-21 22:25:09 +0100

    ajout d'espace
    description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>EDUVAULT</title>
+        <title>EDUVAULT </title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -18,7 +18,7 @@
         @livewireStyles
     </head>
     <body class="font-sans antialiased">
-    
+
         <x-banner />
 
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">

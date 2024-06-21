88c59a1c3add229908705d5bb581724b059d221e
Diandreas <127276266+Diandreas@users.noreply.github.com>
2024-06-21 20:06:03 +0100

    fix

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -6,14 +6,17 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-mark class="block h-9 w-auto" />
+{{--                        <x-application-mark class="block h-9 w-auto" />--}}
+                        <b style="color: blanchedalmond;">EDUVAULT</b>
+
+
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                        {{ __('ACCEUIL') }}
                     </x-nav-link>
                 </div>
             </div>
